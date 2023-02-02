d7547f7f47cd1bd8a0d47bd6562bc74f3cbeccd4
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2023-02-02 16:25:46 -0400

    Add physics mask

---
+++ SpriteChicken/SpriteChicken/Tools/PhysicsMask.swift
@@ -0,0 +1,44 @@
+//
+//  PhysicsMask.swift
+//  SpriteChicken
+//
+//  Created by Victor Vasconcelos on 02/02/23.
+//
+
+import Foundation
+import SpriteKit
+
+extension UInt32 {
+    static let base: UInt32 = 0b1
+    static let player = UInt32.base << 0
+    static let enemy = UInt32.base << 1
+    static let items = UInt32.base << 2
+    static let ground = UInt32.base << 3
+    static let wall = UInt32.base << 4
+    static let enemyWall = UInt32.base << 5
+    static let trigger = UInt32.base << 6
+    static let trap = UInt32.base << 7
+    
+    static let allMasks: [UInt32] = [
+        .player,
+        .enemy,
+        .items,
+        .ground,
+        .wall,
+        .enemyWall,
+        .trigger,
+        .trap
+    ]
+    
+    static func contactWithAllCategories(less: [UInt32] = []) -> UInt32 {
+        var result: UInt32 = 0b00
+        
+        for category in UInt32.allMasks {
+            if !less.contains(category) {
+                result |= category
+            }
+        }
+        
+        return result
+    }
+}
